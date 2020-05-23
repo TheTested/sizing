67ae49d87f5d182fbf07f137737faca48383d26a
TheTested <jomajoma12@gmail.com>
2020-05-23 18:33:14 +0300

    Update size.js

--- size.js
+++ size.js
@@ -39,13 +39,13 @@ pageHeight = document.body.getBoundingClientRect().height;
     } else {
 	 $(".peopleleft").css(
   {
-  'top':($("[id='imageleft']").offset().top -$(".people").offset().top),
+  'top':($("[id='imageleft']").offset().top + $("[id='imageleft']").height() - $(".peopleleft").height()  -$(".people").offset().top),
   'left':(0)
   });
   
    $(".peopleright").css(
   {
-  'top':($("[id='imageleft']").offset().top -$(".people").offset().top),
+  'top':($("[id='imageleft']").offset().top + $("[id='imageleft']").height() - $(".peopleleft").height()  -$(".people").offset().top),
   'right':(0)
   });  
     }
